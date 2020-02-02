dc729f60a2e7f794cca5125362a7618a0c629e45
cielavenir <cielartisan@gmail.com>
2020-02-03 00:47:55 +0900

    Added Array char16_t specialization to avoid va_arg crash

--- build/output/common.h
+++ build/output/common.h
@@ -62,6 +62,18 @@ template<typename T> struct Array_ : public Ref_ {
 	int64_t L;
 	T* B;
 };
+template<>
+Array_<char16_t>::Array_(int64_t n, ...) noexcept : Ref_() {
+	L = n;
+	B = new char16_t[static_cast<size_t>(n + bufLen_<char16_t>())];
+	va_list l;
+	va_start(l, n);
+	for (int64_t i = 0; i < n; i++)
+		B[i] = va_arg(l, int);
+	va_end(l);
+	if (bufLen_<char16_t>() > 0)
+		B[n] = 0;
+}
 template<typename T> struct List_ : public Ref_ {
 	List_() noexcept : Ref_(), B(), I(B.end()) {}
 	int64_t Len() noexcept { return static_cast<int64_t>(B.size()); }
