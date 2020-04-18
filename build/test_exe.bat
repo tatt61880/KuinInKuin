11699d997bbb81cd6cc2e44f2aa41d0c562b1dd6
kuina <kuinatesso@gmail.com>
2020-04-18 12:42:39 +0900

    A trivial change.

--- build/test_exe.bat
+++ build/test_exe.bat
@@ -2,7 +2,7 @@
 setlocal
 pushd "%~dp0"
 
-.\kuincl.exe -i "%~dp0test_data/test_exe.kn" -o "%~dp0output/test_exe_correct.exe" -s "%~dp0./sys/" -e cui
+.\kuincl.exe -i "%~dp0test_data/test_exe.kn" -o "%~dp0output/exe_correct/test_exe_correct.exe" -s "%~dp0./sys/" -e cui
 
 .\kuincl.exe -i "%~dp0../src/compiler/main.kn" -o "%~dp0output/kuin.exe" -s "%~dp0./sys/" -e cui
 
@@ -10,9 +10,7 @@ pushd "%~dp0"
 
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "%~dp0output/kuin_cpp.sln" /t:clean;rebuild /p:Configuration="Release" /p:Platform="x64" /m
 
-.\output\x64\Release\kuin_cpp.exe -i "%~dp0test_data/test_exe.kn" -o "%~dp0output/test_exe" -s "%~dp0../src/compiler/res/sys/" -e exe
-
-rem .\output\kuin.exe -i "%~dp0test_data/test_exe.kn" -o "%~dp0output/test_exe.exe" -s "%~dp0../src/compiler/res/sys/" -e exe
+.\output\x64\Release\kuin_cpp.exe -i "%~dp0test_data/test_exe.kn" -o "%~dp0output/exe_output/test_exe" -s "%~dp0../src/compiler/res/sys/" -e exe
 
 pause
 
