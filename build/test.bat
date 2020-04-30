a8fd557a8fe76023679f48e01c90643eda94d563
kuina <kuinatesso@gmail.com>
2020-04-30 23:39:57 +0900

    A trivial change.

--- build/test.bat
+++ build/test.bat
@@ -5,17 +5,6 @@ pushd "%~dp0"
 rem Kuin2.kn ->[Kuin1.exe]-> Kuin2.exe
 .\kuincl.exe -i "%~dp0../src/compiler/main.kn" -o "%~dp0output/kuin.exe" -s "%~dp0./sys/" -e cui
 
-rem ---------------------------------------------------------------------------
-
-.\output\kuin.exe -i "%~dp0../src/compiler/main.kn" -o "%~dp0output/kuin_exe" -s "%~dp0../src/compiler/res/sys/" -e exe
-
-.\output\kuin_exe.exe -i "%~dp0../src/compiler/main.kn" -o "%~dp0output/kuin_exe2" -s "%~dp0../src/compiler/res/sys/" -e exe
-
-.\output\kuin_exe2.exe -i "%~dp0test_data/test.kn" -o "%~dp0output/test_kuin_exe" -s "%~dp0../src/compiler/res/sys/" -e exe
-
-.\output\test_kuin_exe.exe > .\output\test_kuin_exe_output.txt
-fc /N "%~dp0output\test_kuin_exe_output.txt" "%~dp0output\test_kuin_exe_correct.txt"
-pause
 
 rem ---------------------------------------------------------------------------
 
