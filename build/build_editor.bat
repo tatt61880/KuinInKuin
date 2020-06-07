ab32efb432bb533421dbf383b12b7a94e7f3e15d
kuina <kuinatesso@gmail.com>
2020-06-08 03:50:11 +0900

    Add kuin_dll.

--- build/build_editor.bat
+++ build/build_editor.bat
@@ -3,7 +3,7 @@ setlocal
 pushd "%~dp0"
 
 .\deploy_exe\kuin.exe -i "%~dp0../src/kuin_editor/kuin_editor.kn" -o "%~dp0deploy_exe/editor" -s "%~dp0deploy_exe/sys/" -e exe -r
-
+copy /Y ".\output\x64\Release\kuin_dll.dll" "%~dp0deploy_exe\data\d0917.knd"
 pause
 
 popd
---
+++ build/output/kuin_dll_main.cpp
@@ -0,0 +1,17 @@
+#define STRICT
+#define _WIN32_DCOM
+#define _USE_MATH_DEFINES
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCKAPI_
+#include <Windows.h>
+
+#define UNUSED(var) (void)(var)
+#define EXPORT_CPP extern "C" _declspec(dllexport)
+
+BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID reserved)
+{
+	UNUSED(hinst);
+	UNUSED(reason);
+	UNUSED(reserved);
+	return TRUE;
+}
