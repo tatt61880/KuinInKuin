1159fe3315c27b2b425b7992c266196181e29a86
kuina <kuinatesso@gmail.com>
2020-06-08 09:05:32 +0900

    A trivial change.

--- build/test.bat
+++ build/test.bat
@@ -26,14 +26,14 @@ rem Kuin2.cpp ->[Visual Studio]->Kuin2.exe
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "%~dp0output/kuin_cpp.sln" /t:clean;rebuild /p:Configuration="Release" /p:Platform="x64" /m
 
 rem Kuin2.kn ->[Kuin2.exe]-> Kuin2.cpp
-.\output\x64\Release\kuin_cpp.exe -i "%~dp0../src/compiler/main.kn" -o "%~dp0output/kuin_cpp2" -s "%~dp0output/test_res/sys/" -e cpp -x nogc
+.\output\x64\Release\kuin_cpp\kuin_cpp.exe -i "%~dp0../src/compiler/main.kn" -o "%~dp0output/kuin_cpp2" -s "%~dp0output/test_res/sys/" -e cpp -x nogc
 
 rem Compare Kuin2.cpp
 fc /N "%~dp0output\kuin_cpp.cpp" "%~dp0output\kuin_cpp2.cpp"
 pause
 
 rem Test Kuin2.exe
-.\output\x64\Release\kuin_cpp.exe -i "%~dp0test_data/test.kn" -o "%~dp0output/test_kuin_cpp" -s "%~dp0output/test_res/sys/" -e cpp
+.\output\x64\Release\kuin_cpp\kuin_cpp.exe -i "%~dp0test_data/test.kn" -o "%~dp0output/test_kuin_cpp" -s "%~dp0output/test_res/sys/" -e cpp
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "%~dp0output/test_kuin_cpp.sln" /t:clean;rebuild /p:Configuration="Release" /p:Platform="x64" /m
 .\output\x64\Release\test_kuin_cpp.exe > .\output\test_kuin_cpp_output.txt
 fc /N "%~dp0output\test_kuin_cpp_output.txt" "%~dp0output\test_kuin_cpp_correct.txt"
@@ -42,7 +42,7 @@ pause
 rem ---------------------------------------------------------------------------
 
 rem Kuin2.kn ->[Kuin2.exe]-> Kuin2.js
-.\output\x64\Release\kuin_cpp.exe -i "%~dp0../src/compiler/main.kn" -o "%~dp0output/kuin_js" -s "%~dp0output/test_res/sys/" -p "%~dp0output/test_res/" -e web
+.\output\x64\Release\kuin_cpp\kuin_cpp.exe -i "%~dp0../src/compiler/main.kn" -o "%~dp0output/kuin_js" -s "%~dp0output/test_res/sys/" -p "%~dp0output/test_res/" -e web
 
 rem Kuin2.js ->[Kuin2.js]-> Kuin2.js
 "C:\Program Files\Mozilla Firefox\firefox.exe" ".\output\kuin_js.html?-i&res/main.kn&-o&kuin_js2&-s&res/sys/&-e&web"
